b3d0b4731af5c2ba8bfee69edc4bdbef997495a3
Benvinda Vedes <bafonso.vedes@gmail.com>
2020-10-29 09:13:54 +0000

    Updates

---
+++ precache-manifest.1c6d76d9982d2cea747807f54b77d577.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec20eb1c7ffd54466c932f702b271d0",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "61b4f1abe3abf3d68599",
+    "url": "/drum-machine/static/css/main.8533b467.chunk.css"
+  },
+  {
+    "revision": "c3bece19082bc0c353fe",
+    "url": "/drum-machine/static/js/2.7389f316.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/drum-machine/static/js/2.7389f316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b4f1abe3abf3d68599",
+    "url": "/drum-machine/static/js/main.9bbe6cb1.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/drum-machine/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/drum-machine/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "00dbf3ed74421d9d7b54b5a57c7a397e",
+    "url": "/drum-machine/static/media/drums.00dbf3ed.jpg"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/drum-machine/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/drum-machine/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/drum-machine/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/drum-machine/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/drum-machine/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/drum-machine/static/media/tink.2a6bb6f5.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/drum-machine/static/media/tom.572ed39d.wav"
+  }
+]);
